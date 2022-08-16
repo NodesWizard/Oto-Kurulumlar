1b7d76c3b9347c2e06593cd0266007bfa6bf3dd5
Nodes Wizard <110974342+NodesWizard@users.noreply.github.com>
2022-08-17 01:07:17 +0300

    Update haqqs.sh

--- haqqs.sh
+++ haqqs.sh
@@ -160,12 +160,37 @@ sudo systemctl daemon-reload
 sudo systemctl enable $HAQQ
 sudo systemctl restart $HAQQ
 
-
-
 source $HOME/.bash_profile
+sleep 2
+systemctl stop haqqd
+sleep 1
+haqqd tendermint unsafe-reset-all --home $HOME/.haqqd
 
 
+#!/bin/bash
 
+SNAP_RPC="https://rpc.tm.testedge.haqq.network:443"
+
+LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 1000)); \
+TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+
+# persistent_peers
+P_PEERS=""
+
+# seed nodes
+SEEDS="ddc217640ab137ad6f9cf11fd94fba02eb1e1972@seed1.testedge.haqq.network:26656,7028d26e4d37506b4d5e1f668c945a93693d111b@seed2.testedge.haqq.network:26656,8f7b0add0523ec3648cb48bc12ac35357b1a73ae@195.201.123.87:26656,899eb370da6930cf0bfe01478c82548bb7c71460@34.90.233.163:26656,f2a78c20d5bb567dd05d525b76324a45b5b7aa28@34.90.227.10:26656,4705cf12fb56d7f9eb7144937c9f1b1d8c7b6a4a@34.91.195.139:26656"
+sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
+s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
+s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
+s|^(persistent_peers[[:space:]]+=[[:space:]]+).*$|\1\"$P_PEERS\"| ; \
+s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"$SEEDS\"|" $HOME/.haqqd/config/config.toml
+
+
+systemctl restart haqqd
+
+journalctl -fu haqqd -o cat
 echo '=============== KURULUM TAMAM! Nodeist Katkılarıyla ==================='
 echo '=============== www.NodesWizard.com ==================='
 echo -e 'LOGLARI KONTROL ET: \e[1m\e[32mjjournalctl -fu haqqd -o cat\e[0m'
