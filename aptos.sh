9f3c12b8bc235ad0906aab439c0b4c495d5f3d6c
Nodes Wizard <110974342+NodesWizard@users.noreply.github.com>
2022-08-20 23:04:09 +0300

    Update aptos.sh

--- aptos.sh
+++ aptos.sh
@@ -9,58 +9,78 @@ echo -e "\e[0m"
 
 sleep 2
 
+sudo ufw enable -y
+
+sleep 2
+
+sudo ufw allow 8080
+sudo ufw allow 80
+sudo ufw allow 22
+sudo ufw allow 9101
+
+# set vars
+if [ ! $NODENAME ]; then
+	read -p "Enter node name: " NODENAME
+	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+fi
+echo "export WORKSPACE=testnet" >> $HOME/.bash_profile
+echo "export PUBLIC_IP=$(curl -s ifconfig.me)" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+echo -e "\e[1m\e[32m1. Updating dependencies... \e[0m" && sleep 1
 sudo apt update && sudo apt upgrade -y
 
-sleep 2
-
+echo -e "\e[1m\e[32m2. Installing required dependencies... \e[0m" && sleep 1
 sudo apt-get install jq unzip -y
+sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.23.1/yq_linux_amd64 && chmod +x /usr/local/bin/yq
 
-sleep 2
+echo -e "\e[1m\e[32m3. Checking if Docker is installed... \e[0m" && sleep 1
+if ! command -v docker &> /dev/null
+then
+    echo -e "\e[1m\e[32m3.1 Installing Docker... \e[0m" && sleep 1
+    sudo apt-get install ca-certificates curl gnupg lsb-release -y
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+fi
 
-sudo apt-get install ca-certificates curl gnupg lsb-release -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+echo -e "\e[1m\e[32m4. Checking if Docker Compose is installed ... \e[0m" && sleep 1
+docker-compose version
+if [ $? -ne 0 ]
+then
+    echo -e "\e[1m\e[32m4.1 Installing Docker Compose... \e[0m" && sleep 1
+	docker_compose_version=$(wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name")
+	sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m`"
+	sudo chmod +x /usr/bin/docker-compose
+fi
 
-sleep 2
-
-docker_compose_version=$(wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name")
-sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m`"
-sudo chmod +x /usr/bin/docker-compose
-
-sleep 2
-
+# download aptos-cli
 wget -qO aptos-cli.zip https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v0.3.1/aptos-cli-0.3.1-Ubuntu-x86_64.zip
 sudo unzip -o aptos-cli.zip -d /usr/local/bin
 chmod +x /usr/local/bin/aptos
 rm aptos-cli.zip
 
-sleep 2
-
+echo -e "\e[1m\e[32m5. Installing Validator Node ... \e[0m" && sleep 1
 mkdir ~/$WORKSPACE && cd ~/$WORKSPACE
 
-sleep 2
-
+# download configs
 wget -qO docker-compose.yaml https://raw.githubusercontent.com/aptos-labs/aptos-core/main/docker/compose/aptos-node/docker-compose.yaml
 wget -qO validator.yaml https://raw.githubusercontent.com/aptos-labs/aptos-core/main/docker/compose/aptos-node/validator.yaml
 
-sleep 2
-
+# generate keys
 aptos genesis generate-keys --output-dir ~/$WORKSPACE/keys
 
-sleep 2
-
+# configure validator
 aptos genesis set-validator-configuration \
     --local-repository-dir ~/$WORKSPACE \
     --username $NODENAME \
     --owner-public-identity-file ~/$WORKSPACE/keys/public-keys.yaml \
     --validator-host $PUBLIC_IP:6180 \
     --stake-amount 100000000000000
-    
-    sleep 2
-    
-    sudo tee layout.yaml > /dev/null <<EOF
+  
+# add layout file
+sudo tee layout.yaml > /dev/null <<EOF
 ---
 root_key: "D04470F43AB6AEAA4EB616B72128881EEF77346F2075FFE68E14BA7DEBD8095E"
 users:
@@ -79,18 +99,27 @@ voting_duration_secs: 43200
 voting_power_increase_limit: 20
 EOF
 
-sleep 2
-
+# download aptos framework
 wget -qO framework.mrb https://github.com/aptos-labs/aptos-core/releases/download/aptos-framework-v0.3.0/framework.mrb -P ~/$WORKSPACE
 
-sleep 2
-
+# compile genesis blob and waypoint
 aptos genesis generate-genesis --local-repository-dir ~/$WORKSPACE --output-dir ~/$WORKSPACE
 
-sleep 2
-
+# run docker compose
 docker-compose up -d
 
+
+echo -e "\e[1m\e[32mAptos Validator Node BASLADI \e[0m"
+echo -e "Please backup key files located in \e[1m\e[32m~/$WORKSPACE/keys\e[0m and \e[1m\e[32m~/$WORKSPACE/$NODENAME \e[0m"
+
+echo -e "\e[1m\e[32m Senkranizasyonu kontrol et : \e[0m" 
+echo -e "\e[1m\e[39m    curl 127.0.0.1:9101/metrics 2> /dev/null | grep aptos_state_sync_version | grep type \n \e[0m" 
+
+echo -e "\e[1m\e[32m LOGLARI KONTROL ET: \e[0m" 
+echo -e "\e[1m\e[39m    docker logs -f testnet-validator-1 --tail 50 \n \e[0m" 
+
+echo -e "\e[1m\e[32m KAYIT ICIN ANAHTARLARINI BU KODLA AL... www.nodeswizard.com : \e[0m" 
+echo -e "\e[1m\e[39m    cat ~/$WORKSPACE/keys/public-keys.yaml \n \e[0m" 
 
 
 
