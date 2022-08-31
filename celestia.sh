64f91a2e901e42f8aad4dc864392d13c39a96ac8
Nodes Wizard <110974342+NodesWizard@users.noreply.github.com>
2022-08-31 07:49:25 +0300

    Update celestia.sh

--- celestia.sh
+++ celestia.sh
@@ -38,12 +38,24 @@ sleep 1
 
 cd $HOME
 rm -rf celestia-app
+
+sleep 1
+
 git clone https://github.com/celestiaorg/celestia-app.git
+
+sleep 1
+
 cd celestia-app/
 APP_VERSION=$(curl -s \
   https://api.github.com/repos/celestiaorg/celestia-app/releases/latest \
   | jq -r ".tag_name")
+  
+sleep 1
+
 git checkout tags/$APP_VERSION -b $APP_VERSION
+
+sleep 1
+
 make install
 
 sleep 1
@@ -69,19 +81,33 @@ sed -i.bak -e "s/^timeout-commit *=.*/timeout-commit = \"25s\"/" $HOME/.celestia
 sed -i.bak -e "s/^skip-timeout-commit *=.*/skip-timeout-commit = false/" $HOME/.celestia-app/config/config.toml
 sed -i.bak -e "s/^mode *=.*/mode = \"validator\"/" $HOME/.celestia-app/config/config.toml
 
+sleep 1
+
 wget -O $HOME/.celestia-app/config/genesis.json "https://raw.githubusercontent.com/celestiaorg/networks/master/mamaki/genesis.json"
+
+sleep 1
 
 BOOTSTRAP_PEERS=$(curl -sL https://raw.githubusercontent.com/celestiaorg/networks/master/mamaki/bootstrap-peers.txt | tr -d '\n') && echo $BOOTSTRAP_PEERS
 sed -i.bak -e "s/^bootstrap-peers *=.*/bootstrap-peers = \"$BOOTSTRAP_PEERS\"/" $HOME/.celestia-app/config/config.toml
 
+sleep 1
+
 PEERS="7145da826bbf64f06aa4ad296b850fd697a211cc@176.57.189.212:26656, f7b68a491bae4b10dbab09bb3a875781a01274a5@65.108.199.79:20356, 853a9fbb633aed7b6a8c759ba99d1a7674b706a3@38.242.216.151:26656, 96995456b7fe3ab6524fc896dec76d9ba79d420c@212.125.21.178:26656, 268694eaf9446b2052b1161979bf2e09f3e45e81@173.212.254.166:26656, 28aaa8865f3e9bba69f257b08d5c28091b5b3167@176.57.150.79:26656"
   
+sleep 1
+
 sed -i.bak -e "s/^persistent-peers *=.*/persistent-peers = \"$PEERS\"/" $HOME/.celestia-app/config/config.toml
 
 sleep 1
 
 sed -i.bak -e "s/^timeout-commit *=.*/timeout-commit = \"25s\"/" $HOME/.celestia-app/config/config.toml
+
+sleep 1
+
 sed -i.bak -e "s/^skip-timeout-commit *=.*/skip-timeout-commit = false/" $HOME/.celestia-app/config/config.toml
+
+sleep 1
+
 sed -i.bak -e "s/^mode *=.*/mode = \"validator\"/" $HOME/.celestia-app/config/config.toml
 
 sleep 1
@@ -115,7 +141,13 @@ sleep 1
 
 cd $HOME
 rm -rf ~/.celestia-app/data
+
+sleep 1
+
 mkdir -p ~/.celestia-app/data
+
+sleep 1
+
 SNAP_NAME=$(curl -s https://snaps.qubelabs.io/celestia/ | \
     egrep -o ">mamaki.*tar" | tr -d ">")
 wget -O - https://snaps.qubelabs.io/celestia/${SNAP_NAME} | tar xf - \
