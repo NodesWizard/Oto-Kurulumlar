56ddb30669e95c5e5b1afe452e9373956e86f8e2
Nodes Wizard <110974342+NodesWizard@users.noreply.github.com>
2022-10-08 04:58:11 +0300

    Update newrl.sh

--- newrl.sh
+++ newrl.sh
@@ -7,10 +7,9 @@ echo " | |\  | |__| | |__| | |____ ____) |  \  /\  /   _| |_ / /__ / ____ \| | \
 echo " |_| \_|\____/|_____/|______|_____/    \/  \/   |_____/_____/_/    \_\_|  \_\_____/   ";
 echo -e "\e[0m"  
 
-sleep 2
+sleep 1
 
 sudo ufw allow 8421
-sudo ufw allow 8420
 
 sleep 1
 
@@ -19,14 +18,6 @@ sudo apt-get update && sudo apt-get upgrade -y
 sleep 1
 
 git clone https://github.com/git/git
-
-sleep 1
-
-sudo apt install -y build-essential libssl-dev libffi-dev git curl screen -y
-
-sleep 1
-
-sudo apt install screen -y
 
 sleep 1
 
@@ -42,6 +33,10 @@ sleep 2
 git clone https://github.com/newrlfoundation/newrl.git
 cd newrl
 scripts/install.sh testnet
+
+sleep 1
+
+sudo apt install screen -y
 
 
 
